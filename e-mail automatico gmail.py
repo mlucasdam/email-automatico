9102321fabe0539e5bf7faf06f3003ac746213b3
Lucas Damiani <104548614+mlucasdam@users.noreply.github.com>
2022-05-04 11:10:35 -0300

    Update e-mail automatico gmail.py

--- e-mail automatico gmail.py
+++ e-mail automatico gmail.py
@@ -3,22 +3,16 @@ import email.message
 
 
 def enviar_email():
-    corpo_email = """               
-    <p>FORMULA MAGICA PARA FICAR MAIS SEXY: </p>
-    <p>1 - DECA</p>
-    <p>2 - WINSTRON</p>
-    <p>3 - DURATESTON</p>
-    <p>4 - TESTEX</p>
-    <p>ATT</p>
-    <p>BOT DE EMAIL DO MUNABIS</p>
+    corpo_email = """                
+    <p>PARAGRAFO</p> 
     """
-
+# PARA CADA PARAGROFO NOVO COLOCAR <p> </p>
 
     msg = email.message.Message()
-    msg['subject'] = "QUER SABER COMO FICAR MAIS SEXY?"
-    msg['from'] = 'damiani.lucasm@gmail.com'
-    msg['to'] = 'damiani.lucasm@gmail.com;rafael264rodrigues@gmail.com;raqueless@hotmail.com'
-    password = '111275lk'
+    msg['subject'] = "ASSUNTO"
+    msg['from'] = 'REMETENTE' #SEU EMAIL AQUI
+    msg['to'] = 'DESTINATARIO' #EMAIL DESTINO
+    password = 'SENHA' #SENHA DO SEU EMAIL AQUI
     msg.add_header('content-type', 'text/html')
     msg.set_payload(corpo_email)
 
